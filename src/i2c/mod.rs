367f634bdcfcd6eda6466b17f071224d43571d25
AronMaa <aronmaarek@gmail.com>
2025-01-06 23:49:56 +0100

    Update mod.rs

--- src/i2c/mod.rs
+++ src/i2c/mod.rs
@@ -1,1 +1,10 @@
+#[cfg(feature = "atmega328p")]
+pub mod atmega328p;
+#[cfg(feature = "cortex_m7")]
+pub mod cortex_m7;
 
+pub trait I2C {
+    fn init_spi(is_master: bool, baud_rate: u32);
+    fn send_data(data: u8);
+    fn receive_data() -> u8;
+}
