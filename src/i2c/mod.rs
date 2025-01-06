b79f4763bc2b0e28dd033ecb31e19eb9a56173e8
AronMaa <aronmaarek@gmail.com>
2025-01-06 23:55:05 +0100

    Update mod.rs

--- src/i2c/mod.rs
+++ src/i2c/mod.rs
@@ -4,7 +4,7 @@ pub mod atmega328p;
 pub mod cortex_m7;
 
 pub trait I2C {
-    fn init_spi(is_master: bool, baud_rate: u32);
-    fn send_data(data: u8);
-    fn receive_data() -> u8;
+    fn init_i2c(&self);
+    fn send_data(&self, address: u8, data: &[u8]);
+    fn receive_data(&self, address: u8, num_bytes: usize) -> Vec<u8>;
 }
