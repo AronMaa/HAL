842db205d6ca997c06c92ed976a6b21bbd1ebcdc
AronMaa <aronmaarek@gmail.com>
2025-01-06 23:59:45 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -4,19 +4,23 @@
 #[cfg(feature = "atmega328p")]
 use spi::atmega328p::{init_spi, send_data, receive_data};
 use usart::atmega328p::Atmega328pUsart;
+use i2c::atmega328p::{Atmega328pI2c};
 
 #[cfg(feature = "cortex_m7")]
 use spi::cortex_m7::{init_spi, send_data, receive_data};
 use usart::cortex_m7::CortexM7Usart;
+use i2c::cortex_m7::{CortexM7I2c};
 
 mod panic;
 mod gpio;
 mod usart;
 mod spi;
+mod i2c;
 
 use gpio::{DigitalPin, Port, PinMode};
 use usart::Usart;
 use spi::Spi;
+use i2c::I2C;
 
 #[no_mangle]
 pub extern "C" fn main() {
@@ -31,6 +35,17 @@ pub extern "C" fn main() {
         CortexM7Usart::init(9600);
     }
 
+    // Initialize I2C communication
+    #[cfg(feature = "atmega328p")]
+    {
+        Atmega328pI2C::init_i2c(100_000);
+    }
+
+    #[cfg(feature = "cortex_m7")]
+    {
+        CortexM7I2C::init_i2c(&mut i2c);
+    }
+    
     // Configure GPIO pins
     let pin2 = DigitalPin::new(Port::D, 2);
     pin2.configure(PinMode::InputPullUp);
@@ -59,6 +74,14 @@ pub extern "C" fn main() {
     send_data(0x55);
     match receive_data() {
         data => debug_log(&format!("SPI data received: {}", data)),
+    }
+
+    // I2C Communication
+    #[cfg(feature = "atmega328p")]
+    {
+        Atmega328pI2c::send_data(0x42);
+        let received_data = Atmega328pI2c::receive_data();
+        debug_log(&format!("I2C data received: {}", received_data));
     }
 }
 
